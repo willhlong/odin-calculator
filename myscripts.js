cfe1c27f5ac7df2b96fa72549142330298606f23
William Long <willhlong@gmail.com>
2023-01-31 23:13:19 -0500

    Add, subtract, multiply, and divide now function

--- myscripts.js
+++ myscripts.js
@@ -1,40 +1,90 @@
-let buttons = document.querySelectorAll(".button");
-buttons.forEach(button => {
+let calculatedValue = 0;
+let userInput = "";
+let expression = [];
+let numberButtons = document.querySelectorAll(".num-button");
+let opButtons = document.querySelectorAll(".op-button");
+
+numberButtons.forEach(button => {
     button.addEventListener('click', () => {
-        alert("You clicked a button...");
+        userInput += button.textContent;
+        displayValue(userInput);
     })
 });
 
-let calculatedValue = 0;
+opButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        let op = button.textContent;
+        if (button.textContent === '=') {
+            if (expression.length == 2) {
+                expression.push(userInput);
+                operate(expression[1], expression[0], expression[2]);
+            }
+            else {
+                displayValue("error");
+            }
+        }
+        else {
+            expression.push(userInput);
+            expression.push(op);
+            displayExpression(expression);
+            displayValue(0);
+        }
+    })
+})
 
 function add(num1, num2) {
-    return num1 + num2;
+    return Number(num1) + Number(num2);
 }
 
 function sub(num1, num2) {
-    return num1 - num2;
+    return Number(num1) - Number(num2);
 }
 
 function multiply(num1, num2) {
-    return num1 * num2;
+    return Number(num1) * Number(num2);
 }
 
 function divide(num1, num2) {
-    return num1 / num2;
+    return Number(num1) / Number(num2);
 }
 
 function operate(op, num1, num2) {
-    if (op === 'ADD') {
+    if (op === '+') {
         calculatedValue = add(num1, num2);
     }
-    if (op === 'SUB') {
+    if (op === '-') {
         calculatedValue = sub(num1, num2);
     }
-    if (op === 'MULT') {
+    if (op === '*') {
         calculatedValue = multiply(num1, num2);
     }
-    if (op === 'DIV') {
+    if (op === '/') {
         calculatedValue = divide(num1, num2);
     }
-    document.querySelector(".display").textContent = calculatedValue;
+    expression.push("=");
+    displayValue(calculatedValue);
+    displayExpression(expression);
+    userInput = calculatedValue;
+    expression = [];
+}
+
+function displayValue(displayValue) {
+    resetValueDisplay();
+    document.querySelector(".display-value").textContent = displayValue;
+}
+
+function displayExpression(expression) {
+    resetExpressionDisplay();
+    for (let i = 0; i < expression.length; i++) {
+        document.querySelector(".display-expression").textContent += expression[i];
+    }
+    userInput = "";
+}
+
+function resetValueDisplay() {
+    document.querySelector(".display-value").textContent = "";
+}
+
+function resetExpressionDisplay() {
+    document.querySelector(".display-expression").textContent = "";
 }
