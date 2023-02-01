ff296e83b0023bcdfcadf0c7f05a09e11bd94763
William Long <willhlong@gmail.com>
2023-01-31 21:16:40 -0500

    Added add, sub, multiply, and divide functions

--- myscripts.js
+++ myscripts.js
@@ -4,3 +4,37 @@ buttons.forEach(button => {
         alert("You clicked a button...");
     })
 });
+
+let calculatedValue = 0;
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function sub(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operate(op, num1, num2) {
+    if (op === 'ADD') {
+        calculatedValue = add(num1, num2);
+    }
+    if (op === 'SUB') {
+        calculatedValue = sub(num1, num2);
+    }
+    if (op === 'MULT') {
+        calculatedValue = multiply(num1, num2);
+    }
+    if (op === 'DIV') {
+        calculatedValue = divide(num1, num2);
+    }
+    document.querySelector(".display").textContent = calculatedValue;
+}
